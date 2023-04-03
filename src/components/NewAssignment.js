fd44f38b061cde37ff69ca2d37fa9119a927af81
Georgekorolev <gkorolev@csumb.edu>
2023-04-02 21:40:29 -0700

    Added rendering to the NewAssignment page

--- src/components/NewAssignment.js
+++ src/components/NewAssignment.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import Button from '@mui/material/Button';
 import Radio from '@mui/material/Radio';
+import Grid from '@mui/material/Grid';
 import {DataGrid} from '@mui/x-data-grid';
 import {SERVER_URL} from '../constants.js';
 
@@ -16,8 +17,31 @@ class NewAssignment extends React.Component {
   constructor(props) {
     super(props);
   };
- 
   
+  render() {
+    return(
+         <div className="App">
+           <Grid container>
+             <Grid item align="left">
+                <h4>Assignment: </h4>
+                <h4>Course: </h4>                   
+             </Grid>
+           </Grid>
+           <div style={{width:'100%'}}>
+             For DEBUG:  display state.
+             {JSON.stringify(this.state)}
+           </div>
+           <div style={{ height: 400, width: '100%' }}>
+              <Button component={Link} to={{pathname:'/'}} id="Back" variant="outlined" color="primary" style={{margin: 10}} >
+               Back
+              </Button>
+              <Button id="Submit" variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleSubmit} >
+               Submit
+              </Button>
+           </div> 
+         </div>
+    ); 
+  };
 }  
 
 export default NewAssignment;
